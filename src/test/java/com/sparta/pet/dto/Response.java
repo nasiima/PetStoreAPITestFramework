a865b5b45fbe4250a46da9c13d05606983f977d8
Alena Krasheninnikova <alenakrash@mail.ru>
2022-11-21 17:08:50 +0300

    dsds

--- src/test/java/com/sparta/pet/dto/Response.java
+++ src/test/java/com/sparta/pet/dto/Response.java
@@ -1,13 +1,26 @@
 package com.sparta.pet.dto;
 
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class PetResponse {
+public class Response{
+
+	@JsonProperty("photoUrls")
 	private List<String> photoUrls;
+
+	@JsonProperty("name")
 	private String name;
+
+	@JsonProperty("id")
 	private int id;
+
+	@JsonProperty("category")
 	private Category category;
+
+	@JsonProperty("tags")
 	private List<TagsItem> tags;
+
+	@JsonProperty("status")
 	private String status;
 
 	public List<String> getPhotoUrls(){
