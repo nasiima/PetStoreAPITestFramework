d81e98ef16af8c29e59ec8c5755914acf5796fab
Alena Krasheninnikova <alenakrash@mail.ru>
2022-11-21 17:25:43 +0300

    sls

--- src/test/java/com/sparta/pet/PetTest.java
+++ src/test/java/com/sparta/pet/PetTest.java
@@ -1,4 +1,16 @@
 package com.sparta.pet;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class PetTest {
+
+    @Test
+    @DisplayName("Status code is correct")
+    public void TestStatusCode(){
+//        RestAssured
+//                .when()
+//                .get("https://www.boredapi.com/api/activity")
+        response.then().assertThat().statusCode(200);
+    }
 }
--- src/test/java/com/sparta/pet/dto/TagsItem.java
+++ src/test/java/com/sparta/pet/dto/TagsItem.java
@@ -1,7 +1,13 @@
 package com.sparta.pet.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class TagsItem{
+
+	@JsonProperty("name")
 	private String name;
+
+	@JsonProperty("id")
 	private int id;
 
 	public String getName(){
