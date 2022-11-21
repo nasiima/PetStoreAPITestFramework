8a1e15c3c20d86af1095a53f5a7980267451df34
Alena Krasheninnikova <alenakrash@mail.ru>
2022-11-21 17:02:41 +0300

    sas

---
+++ src/test/java/com/sparta/pet/dto/Category.java
@@ -0,0 +1,14 @@
+package com.sparta.pet.dto;
+
+public class Category{
+	private String name;
+	private int id;
+
+	public String getName(){
+		return name;
+	}
+
+	public int getId(){
+		return id;
+	}
+}
