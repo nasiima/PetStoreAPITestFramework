bffc9141d2153c8710ff843a2e52c7a83b2e2e4f
nastiashpyt <nastiashpyt>
2022-11-21 14:26:16 +0000

    created step definitions folder

---
+++ src/test/java/com/sparta/pet/stepDefinitions/orderStepDefs.java
@@ -0,0 +1,39 @@
+package com.sparta.pet.stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class orderStepDefs {
+    @When("I send post request to {string}")
+    public void iSendPostRequestTo(String URL) {
+    }
+
+    @And("I send Order object as request body")
+    public void iSendOrderObjectAsRequestBody() {
+    }
+
+    @Then("I get response code {int}")
+    public void iGetResponseCode(int code) {
+    }
+
+    @And("I get Order object as response body")
+    public void iGetOrderObjectAsResponseBody() {
+    }
+
+    @And("I send empty request body")
+    public void iSendEmptyRequestBody() {
+    }
+
+    @And("I get an error message {string}")
+    public void iGetAnErrorMessage(String message) {
+    }
+
+    @And("I send empty Order object as a request body")
+    public void iSendEmptyOrderObjectAsARequestBody() {
+    }
+
+    @And("I get Order object with default values")
+    public void iGetOrderObjectWithDefaultValues() {
+    }
+}
