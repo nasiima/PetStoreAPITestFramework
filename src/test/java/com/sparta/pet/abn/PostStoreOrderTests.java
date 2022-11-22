53ef62d2254dfe133d186d9fd4934073a83132e5
anastasiashpyt <anastasiashpyt@gmail.com>
2022-11-22 15:54:12 +0000

    Completed step defs for get Inventory request

--- src/test/java/com/sparta/pet/abn/PostStoreOrderTests.java
+++ src/test/java/com/sparta/pet/abn/PostStoreOrderTests.java
@@ -13,23 +13,26 @@ import static net.serenitybdd.rest.SerenityRest.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 public class PostStoreOrderTests extends UIInteractions {
     @Test
     @DisplayName("Successful order post request")
     public void testOrderPostRequest() {
-        Order order = new Order(10, 198772, 7, LocalDateTime.now().plusDays(1), "approved", true);
+        Order order = new Order(10, 198772, 7, new Date(), "approved", true);
 
-        Order responseOrder = given()
-                .baseUri("https://petstore.swagger.io")
-                .basePath("/api/v3/store/order")
-                .body(order, ObjectMapperType.GSON)
-                .accept(ContentType.JSON)
-                .contentType(ContentType.JSON)
-                .post()
-                .getBody()
-                .as(Order.class, ObjectMapperType.GSON);
-
-        MatcherAssert.assertThat(responseOrder.getId(), equalTo(10));
+        var v1 = given();
+        var v2 = v1.body("{\n" +
+                "  \"id\": 10,\n" +
+                "  \"petId\": 198772,\n" +
+                "  \"quantity\": 7,\n" +
+                "  \"shipDate\": \"2022-11-22T13:55:10.012Z\",\n" +
+                "  \"status\": \"approved\",\n" +
+                "  \"complete\": true\n" +
+                "}");
+        var v3 = v2.accept(ContentType.JSON);
+        var v4 = v3.contentType(ContentType.JSON);
+        var v5 = v4.post("https://petstore3.swagger.io/api/v3/store/order");
+        v5.then().statusCode(200);
     }
 }
