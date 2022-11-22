b78436af661919477d76e01625b2a6f49e241013
pennyadams <penny.adams@live.co.uk>
2022-11-22 13:38:40 +0000

    Login tests complete

--- src/test/java/com/sparta/user/UserTests/LoggingInTests.java
+++ src/test/java/com/sparta/user/UserTests/LoggingInTests.java
@@ -1,5 +1,6 @@
 package com.sparta.user.UserTests;
 
+import com.sparta.user.stepDefinitions.LoggingInStepDefs;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,9 +11,13 @@ public class LoggingInTests {
     @ExtendWith(SerenityJUnit5Extension.class)
     public class loggingIn {
 
+        LoggingInStepDefs loggingInStepDefs;
+
         @Test
         public void logIn() {
-
+            loggingInStepDefs.i_am_a_registered_user();
+            loggingInStepDefs.i_log_in();
+            loggingInStepDefs.the_response_contains("Logged in user session");
         }
 
     }
